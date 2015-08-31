03160ea5cdffda1cb6dce0b1c01e5c94e3d71185
Gregory Nisbet <gregory.nisbet@appdynamics.com>
2015-08-31 12:06:43 -0700

    update god-ergo and defuns

--- configs/god-ergo.el
+++ configs/god-ergo.el
@@ -15,13 +15,20 @@
 (load-modes)
 (load-optional-modes)
 (recentf-mode +1)
-(global-set-key (kbd "C-t") 'previous-line)
-(global-set-key (kbd "C-p") 'transpose-chars)
+;;(global-set-key (kbd "C-t") 'previous-line)
+;;(global-set-key (kbd "C-p") 'transpose-chars)
 (global-set-key (kbd "C-h") (god-extension-set-mode "enable god mode" t))
 (global-set-key (kbd "C-c h") (lookup-key global-map (kbd "<f1>")))
-(define-key god-local-mode-map (kbd " ") 'repeat)
+(global-set-key (kbd "C-c t") 'previous-buffer)
+(global-set-key (kbd "C-c n") 'next-buffer)
 
 (global-set-key (kbd "C-.") 'repeat)
+;; ^R now goes back a line
+;; C-c r for isearch reverse has weird unexpected behavior
+;; like not repeating the search when you'd expect it to
+;; maybe think of better things than C-v and M-v for
+;; scrolling up and down pages.
+;; save ^T for ratpoison
 (ratpoison-compat)
 
 (expression-navigation)
--- lisp/defuns.el
+++ lisp/defuns.el
@@ -1,3 +1,24 @@
+(require 'cl-lib)
+
+(defun log-exception (exception func-name)
+  "log errors to message buffer"
+  (message "logging: EXCEPTION [%s] in %s" exception func-name))
+
+;; not sure if unwind-protect is necessary here.
+;; NOTE docstring is mandatory.
+(defmacro defun* (func args docstring &rest body)
+  "create function that robustly handles failure. docstring required."
+  (cl-assert (stringp docstring))
+  `(defun ,func ,args
+     ,docstring
+     (progn
+       (unwind-protect
+           (condition-case ex
+               (progn ,@body)
+             ('error
+              (log-exception ex ,(symbol-name func))))))))
+
+
 (defmacro god-extension-set-mode (docstring state)
   "create function to set god-mode to specific state"
   `(lambda ()
@@ -5,34 +26,34 @@
      (interactive)
      (setq god-global-mode ,state)
      (if god-global-mode
-       (god-local-mode +1)
+         (god-local-mode +1)
        (god-local-mode -1))))
 
 (defmacro global-window-shortcut (key-string which-window)
   "create global shortcut C-c # for jumping to window #"
   `(global-set-key
-     (kbd ,key-string)
-     (lambda ()
-       (interactive)
-       (window-number-select ,which-window))))
+    (kbd ,key-string)
+    (lambda ()
+      (interactive)
+      (window-number-select ,which-window))))
 
 (defmacro god-extension-set-mode (docstring state)
   "toggle god mode"
   `(lambda ()
      ,docstring
-    (interactive)
-    (setq god-global-mode ,state)
-    (if god-global-mode
-        (god-local-mode +1)
-      (god-local-mode -1))))
+     (interactive)
+     (setq god-global-mode ,state)
+     (if god-global-mode
+         (god-local-mode +1)
+       (god-local-mode -1))))
 
-(defun set-archives ()
+(defun* set-archives ()
   "set the archives"
   (setq package-archives '(("gnu" . "http://elpa.gnu.org/packages/") 
-       ("org" . "http://orgmode.org/elpa/") 
-       ("marmalade" . "http://marmalade-repo.org/packages/") 
-       ("melpa-stable" . "http://melpa-stable.milkbox.net/packages/")
-       ("melpa" . "http://melpa.milkbox.net/packages/"))))
+                           ("org" . "http://orgmode.org/elpa/") 
+                           ("marmalade" . "http://marmalade-repo.org/packages/") 
+                           ("melpa-stable" . "http://melpa-stable.milkbox.net/packages/")
+                           ("melpa" . "http://melpa.milkbox.net/packages/"))))
 
 (defun require-package (package)
   "install package from source"
@@ -61,7 +82,7 @@
   (cl-assert (stringp key)) 
   (define-key current-keymap (kbd key) cmd))
 
-(defun load-modes ()
+(defun* load-modes ()
   "require generally used modes"
 
 
@@ -69,7 +90,7 @@
   (require-package 'paredit)
   (require-package 'evil)
   (require-package 'php-mode)
-
+  
   (require 'god-mode)
   (require 'ace-jump-mode)
   (require 'paredit)
@@ -77,9 +98,15 @@
   (require 'recentf)
   (require 'god-kmacro)
   (require 'keymaps)
+  (require 'org)
   )
 
-(defun load-optional-modes ()
+(defun* load-evil ()
+  "require evil mode"
+  (require-package 'evil)
+  (require 'evil))
+
+(defun* load-optional-modes ()
   "load non mandatory modes, keep start time down"
   (interactive)
 
@@ -113,7 +140,7 @@
   ;; use this symbol to quickly check if optional modes actually loaded
   (setf DEBUG_LOADED_OPTIONAL t))
 
-(defun sensible-defaults ()
+(defun* sensible-defaults ()
   "overwrite some of the default keybindings"
   ;; replace some functions with more useful ones
   ;; some of these are taken from
@@ -138,7 +165,7 @@
   (global-set-key (kbd "M-a") 'backward-paragraph)
   (global-set-key (kbd "M-e") 'forward-paragraph))
 
-(defun window-shortcuts ()
+(defun* window-shortcuts ()
   "set global window shortcuts in C-c map"
   (global-window-shortcut "C-c 1" 1)
   (global-window-shortcut "C-c 2" 2)
@@ -151,17 +178,19 @@
   (global-window-shortcut "C-c 9" 9)
   (global-window-shortcut "C-c 0" 10))
 
-(defun dired-extensions ()
+(defun* dired-extensions ()
+  "dired local map extensions"
   (define-key dired-mode-map (kbd "C-c C-t") 'dired-previous-line)
   (define-key dired-mode-map (kbd "C-c C-d") 'dired-toggle-marks)
   (define-key dired-mode-map (kbd "C-c C-c") 'image-dired-map)
   (define-key dired-mode-map (kbd "b") 'dired-up-directory))
 
-(defun load-private ()
+(defun* load-private ()
+  "load definitions in private elisp file"
   (when (file-exists-p "~/.emacs.d/private.el")
-  (require 'private)))
+    (require 'private)))
 
-(defun setq-config ()
+(defun* setq-config ()
   "miscellaneous setq configuration"
   (setq visible-bell t)
   (setq magit-last-seen-setup-instructions "1.4.0")
@@ -171,31 +200,31 @@
   (setq recentf-max-menu-items 25)
   (add-hook 'haskell-mode-hook 'turn-on-haskell-indentation))
 
-(defun sensible-aliases ()
+(defun* sensible-aliases ()
   "aliases that are sensible"
-  ; http://ergoemacs.org/emacs/emacs_alias.html
+                                        ; http://ergoemacs.org/emacs/emacs_alias.html
   (defalias 'yes-or-no-p 'y-or-n-p)
   (defalias 'list-buffers 'ibuffer)
   (defalias 'perl-mode 'cperl-mode))
 
-(defun sensible-modes ()
+(defun* sensible-modes ()
   "enable sensible modes"
   (recentf-mode +1)
   (show-paren-mode +1))
 
-(defun delayed-load-optional-modes ()
+(defun* delayed-load-optional-modes ()
   "load optional modes if we've been idle for a while"
   (setf load-optional (or (daemonp) window-system))
   (if load-optional (load-optional-modes) (run-with-idle-timer 10 nil #'load-optional-modes)))
 
-(defun dired-changes ()
+(defun* dired-changes ()
   "small changes to dired mode keybindings"
   (define-key dired-mode-map (kbd "C-c C-t") 'dired-previous-line)
   (define-key dired-mode-map (kbd "C-c C-d") 'dired-toggle-marks)
   (define-key dired-mode-map (kbd "C-c C-c") 'image-dired-map)
   (define-key dired-mode-map (kbd "b") 'dired-up-directory))
 
-(defun expression-navigation ()
+(defun* expression-navigation ()
   "commands for navigating balanced expressions"
   ;; expression navigation
   (global-set-key (kbd "C-c c n") 'forward-list)
@@ -208,7 +237,7 @@
   ;;(global-set-key (kbd "C-c c r") 'isearch-backward-regexp)
   (global-set-key (kbd "C-c c space") 'mark-sexp))
 
-(defun god-local-mapify ()
+(defun* god-local-mapify ()
   "define stuff like capital letters in god-local mode"
   (define-key god-local-mode-map (kbd "i") (god-extension-set-mode "disable god mode" nil))
   (define-key god-local-mode-map (kbd "S") 'save-buffer)
@@ -216,9 +245,68 @@
   (define-key god-local-mode-map (kbd "V") 'scroll-down-command)
   (define-key god-local-mode-map (kbd "R") 'recentf-open-files))
 
-(defun ratpoison-compat ()
+(defun* ratpoison-compat ()
   "remap C-r to previous line so as not to conflict with ratpoison"
   (global-set-key (kbd "C-r") 'previous-line)
-  (global-set-key (kbd "C-c r") 'isearch-backward-regexp))
+  (global-set-key (kbd "C-c r") 'isearch-backward-regexp)
+  ;; enable repetition of C-c r to repeat search
+  (define-key isearch-mode-map (kbd "C-c r") 'isearch-repeat-backward))
+
+;; These are the commands mapped in insert mode in evil-mode
+;; most of them are pretty low frequency commands so they will be remapped
+;; to C-c g <whatever>                    
+;; C-d             evil-shift-left-line
+;; C-e             evil-copy-from-below
+;; C-k             evil-insert-digraph
+;; C-n             evil-complete-next
+;; C-o             evil-execute-in-normal-state
+;; C-p             evil-complete-previous
+;; C-r             evil-paste-from-register
+;; C-t             evil-shift-right-line
+;; C-v             quoted-insert
+;; C-w             evil-delete-backward-word
+;; C-x             Prefix Command
+;; C-y             evil-copy-from-above
+;; C-z             evil-emacs-state
+;; <delete>        delete-char
+;; <escape>        evil-normal-state
+;; <remap>         Prefix Command
+;; <remap> <delete-backward-char>  evil-delete-backward-char-and-join
+;; C-x C-n         evil-complete-next-line
+;; C-x C-p         evil-complete-previous-line
+(defun* evil-purge-ctrl-keys ()
+  "eliminate ctrl keys from insert state (making it essentially emacs state, I guess"
+  (define-key evil-insert-state-map (kbd "C-d") nil)
+  (define-key evil-insert-state-map (kbd "C-e") nil)
+  (define-key evil-insert-state-map (kbd "C-k") nil)
+  (define-key evil-insert-state-map (kbd "C-n") nil)
+  (define-key evil-insert-state-map (kbd "C-o") nil)
+  (define-key evil-insert-state-map (kbd "C-p") nil)
+  (define-key evil-insert-state-map (kbd "C-r") nil)
+  (define-key evil-insert-state-map (kbd "C-t") nil)
+  (define-key evil-insert-state-map (kbd "C-v") nil)
+  (define-key evil-insert-state-map (kbd "C-w") nil)
+  (define-key evil-insert-state-map (kbd "C-x") nil)
+  (define-key evil-insert-state-map (kbd "C-y") nil)
+  (define-key evil-insert-state-map (kbd "C-z") nil)
+
+  (global-set-key (kbd "C-c g d") 'evil-shift-left-line)
+  (global-set-key (kbd "C-c g e") 'evil-copy-from-below)
+  (global-set-key (kbd "C-c g k") 'evil-insert-digraph)
+  (global-set-key (kbd "C-c g n") 'evil-complete-next)
+  (global-set-key (kbd "C-c g o") 'evil-execute-in-normal-state)
+  (global-set-key (kbd "C-c g p") 'evil-complete-previous)
+  (global-set-key (kbd "C-c g r") 'evil-paste-from-register)
+  (global-set-key (kbd "C-c g t") 'evil-shift-right-line)
+  (global-set-key (kbd "C-c g v") 'quoted-insert)
+  (global-set-key (kbd "C-c g w") 'evil-delete-backward-word)
+  (global-set-key (kbd "C-c g x n") 'evil-complete-next-line)
+  (global-set-key (kbd "C-c g x p") 'evil-complete-previous-line)
+  (global-set-key (kbd "C-c g y") 'evil-copy-from-above)
+  (global-set-key (kbd "C-c g z") 'evil-emacs-state)
+  
+  
+  
+  )
 
 (provide 'defuns)
